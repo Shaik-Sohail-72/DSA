62c2bb66a3bd297be334d3738bf62e33493d84f3
Shaik Sohail Hussain <shaiksohailhu7n@gmail.com>
2023-02-21 00:19:04 +0530

    Strings - Check whether a String is Palindrome or not

---
+++ p25.java
@@ -0,0 +1,33 @@
+/*Given a string S, check if it is palindrome or not.
+
+Example 1:
+
+Input: S = "abba"
+Output: 1
+Explanation: S is a palindrome
+Example 2:
+
+Input: S = "abc" 
+Output: 0
+Explanation: S is not a palindrome
+ 
+
+Your Task:
+You don't need to read input or print anything. Complete the function isPalindrome()which accepts string S and returns an integer value 1 or 0.
+
+Expected Time Complexity: O(Length of S)
+Expected Auxiliary Space: O(1)*/
+class Solution {
+    int isPalindrome(String S) {
+        int l=0;
+        int r=S.length()-1;
+        while(l<=r){
+            if(S.charAt(l)!=S.charAt(r)){
+                return 0;
+            }
+            l++;
+            r--;
+        }
+        return 1;
+    }
+};
