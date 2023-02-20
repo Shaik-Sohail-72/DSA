af67cedd0cf10b4981bb1c169c87b9ffce7909b4
Shaik Sohail Hussain <shaiksohailhu7n@gmail.com>
2023-02-20 12:22:05 +0530

    Arrays - Find the 'Kth' max element of an array

---
+++ p16.java
@@ -0,0 +1,34 @@
+/*Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+You must solve it in O(n) time complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 105
+-104 <= nums[i] <= 104*/
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> p=new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++){
+            p.add(nums[i]);
+        }
+        for(int i=0;i<k-1;i++){
+            p.poll();
+        }
+        return p.peek();
+    }
+}
