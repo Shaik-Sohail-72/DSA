28c7738a005eea9658167a47869eaaa2dbfbada8
Shaik Sohail Hussain <shaiksohailhu7n@gmail.com>
2023-02-21 17:32:03 +0530

    Arrays - Chocolate Distribution Problem

---
+++ p27.java
@@ -0,0 +1,35 @@
+/*Given an array of N integers where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets such that: 
+
+Each student gets one packet.
+The difference between the number of chocolates in the packet with maximum chocolates and the packet with minimum chocolates given to the students is minimum.
+Examples:
+
+Input : arr[] = {7, 3, 2, 4, 9, 12, 56} , m = 3 
+Output: Minimum Difference is 2 
+Explanation:
+We have seven packets of chocolates and we need to pick three packets for 3 students 
+If we pick 2, 3 and 4, we get the minimum difference between maximum and minimum packet sizes.
+
+Input : arr[] = {3, 4, 1, 9, 56, 7, 9, 12} , m = 5 
+Output: Minimum Difference is 6 
+
+Input : arr[] = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50} , m = 7 
+Output: Minimum Difference is 10 */
+import java.util.*;
+class p27{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int m=sc.nextInt();
+        Arrays.sort(arr);
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n-m+1;i++){
+            min=Math.min(min,arr[i+m-1]-arr[i]);
+        }
+        System.out.println(min);
+    }
+}
