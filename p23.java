9fc6b111fc9182335ebf9c22705ac8d7db8bba4a
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-09 20:28:04 +0530

    Arrays - Two Pointers - Merge Sorted Array

--- p23.java
+++ p23.java
@@ -1,5 +1,4 @@
 /*An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
-
 Examples : 
 
 Input: 
