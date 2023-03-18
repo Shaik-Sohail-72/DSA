3b1bb74d83d438a35769f1fb2e0471b579b50223
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 21:08:39 +0530

    IMP - Arrays - Best time to Buy and Sell Stock

--- p17.java
+++ p17.java
@@ -5,13 +5,13 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
  
-
 Example 1:
 
 Input: prices = [7,1,5,3,6,4]
 Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
 Example 2:
 
 Input: prices = [7,6,4,3,1]
