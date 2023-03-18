b8adfce4f7afd641418de367f3b5bf5427460183
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 21:49:49 +0530

    IMP - Arrays - Chocolate Distribution Problem

--- p27.java
+++ p27.java
@@ -1,5 +1,6 @@
 /*Given an array of N integers where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets such that: 
 
+
 Each student gets one packet.
 The difference between the number of chocolates in the packet with maximum chocolates and the packet with minimum chocolates given to the students is minimum.
 Examples:
