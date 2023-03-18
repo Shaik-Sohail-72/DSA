857cd8ad0c05ba915943d567b1a3d2532a33f679
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 21:11:23 +0530

    IMP - Arrays - Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo

--- p18.java
+++ p18.java
@@ -4,12 +4,12 @@ We will use the integers 0, 1, and 2 to represent the color red, white, and blue
 
 You must solve this problem without using the library's sort function.
 
- 
 
 Example 1:
 
 Input: nums = [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
+
 Example 2:
 
 Input: nums = [2,0,1]
