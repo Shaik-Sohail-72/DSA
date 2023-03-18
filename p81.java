5657bcb6639603b7ced9576379fb70c49e6932c5
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 23:35:48 +0530

    Arrays - Binary Search - Find Target Indices After Sorting Array

--- p81.java
+++ p81.java
@@ -4,7 +4,6 @@ A target index is an index i such that nums[i] == target.
 
 Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
 
- 
 
 Example 1:
 
