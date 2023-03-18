07a13d122867e9c8be1067692ee24049dd9418e9
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 23:42:03 +0530

    Arrays - Bit Manipulation - Counting Bits and store in array

--- p53.java
+++ p53.java
@@ -1,6 +1,5 @@
 /*Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
- 
 
 Example 1:
 
@@ -10,6 +9,7 @@ Explanation:
 0 --> 0
 1 --> 1
 2 --> 10
+
 Example 2:
 
 Input: n = 5
