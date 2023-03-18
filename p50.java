edc7d8d0040b202a959c81dddd95627c9a52193a
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 22:36:33 +0530

    Arrays - Bit Manipulation - Subsets II

--- p50.java
+++ p50.java
@@ -2,12 +2,12 @@
 
 The solution set must not contain duplicate subsets. Return the solution in any order.
 
- 
 
 Example 1:
 
 Input: nums = [1,2,2]
 Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
 Example 2:
 
 Input: nums = [0]
