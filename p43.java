5e0f7f2bfa750ef7fea64ccd3bed1eee12af353c
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 22:26:58 +0530

    IMP - Strings - Write a Code to check whether one string is a rotation of another

--- p43.java
+++ p43.java
@@ -1,5 +1,4 @@
 /*Given two strings s1 and s2. The task is to check if s2 is a rotated version of the string s1. The characters in the strings are in lowercase.
-
  
 
 Example 1:
@@ -29,14 +28,12 @@ rotation s2 can't be obtained by s1.
 Your Task:
 The task is to complete the function areRotations() which checks if the two strings are rotations of each other. The function returns true if string 1 can be obtained by rotating string 2, else it returns false.
 
- 
 
 Expected Time Complexity: O(N).
 Expected Space Complexity: O(N).
 Note: N = |s1|.
 
  
-
 Constraints:
 1 <= |s1|, |s2| <= 107*/
 class Solution{
