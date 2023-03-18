0a6b57f571c45928b8d6d1c16b3d7be029b3ed4e
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 23:23:34 +0530

    Arrays - Two Pointers - Remove Element

--- p62.java
+++ p62.java
@@ -24,7 +24,6 @@ for (int i = 0; i < actualLength; i++) {
 }
 If all assertions pass, then your solution will be accepted.
 
- 
 
 Example 1:
 
@@ -32,6 +31,7 @@ Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 It does not matter what you leave beyond the returned k (hence they are underscores).
+
 Example 2:
 
 Input: nums = [0,1,2,2,3,0,4,2], val = 2
