9ca11bcc35024a575a64429156624245d5a577cf
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 21:06:41 +0530

    IMP - Arrays - Find the 'Kth' max element of an array

--- p16.java
+++ p16.java
@@ -5,11 +5,11 @@ Note that it is the kth largest element in the sorted order, not the kth distinc
 You must solve it in O(n) time complexity.
 
  
-
 Example 1:
 
 Input: nums = [3,2,1,5,6,4], k = 2
 Output: 5
+
 Example 2:
 
 Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
