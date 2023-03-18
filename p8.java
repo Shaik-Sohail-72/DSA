d0287ba7eec47ec4cf7dc99d6d395c90e035364b
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 20:45:46 +0530

    IMP - Strings - Valid Anagram

--- p8.java
+++ p8.java
@@ -3,11 +3,11 @@
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
  
-
 Example 1:
 
 Input: s = "anagram", t = "nagaram"
 Output: true
+
 Example 2:
 
 Input: s = "rat", t = "car"
