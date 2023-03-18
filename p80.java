b403964a060bae6791229288c38f44f302da246f
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-18 23:34:55 +0530

    Matrix - Binary Search - Count Negative Numbers in a Sorted Matrix

--- p80.java
+++ p80.java
@@ -1,7 +1,6 @@
 /*Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 
  
-
 Example 1:
 
 Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
