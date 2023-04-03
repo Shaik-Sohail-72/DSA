857703dd5aca34ccad1c5ca1094e0f0289309aed
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-04-03 23:17:04 +0530

    Trees - Binary Tree - Postorder Traversal

---
+++ p112.java
@@ -0,0 +1,53 @@
+/*Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ls=new ArrayList<Integer>();
+        if(root==null){
+            return ls;
+        }else{
+            ls.addAll(postorderTraversal(root.left));
+            ls.addAll(postorderTraversal(root.right));
+            ls.add(root.val);
+        }
+        return ls;
+    }
+}
