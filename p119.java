dadc5dce5a7a9c410fb3476bb6f7b03cd5df2926
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-04-17 19:17:28 +0530

    Arrays - Stack - Baseball Game

--- p119.java
+++ p119.java
@@ -1,7 +1,5 @@
 /*You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
-
 You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
-
 An integer x.
 Record a new score of x.
 '+'.
@@ -11,13 +9,9 @@ Record a new score that is the double of the previous score.
 'C'.
 Invalidate the previous score, removing it from the record.
 Return the sum of all the scores on the record after applying all the operations.
-
 The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
-
  
-
 Example 1:
-
 Input: ops = ["5","2","C","D","+"]
 Output: 30
 Explanation:
@@ -28,7 +22,6 @@ Explanation:
 "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
 The total sum is 5 + 10 + 15 = 30.
 Example 2:
-
 Input: ops = ["5","-2","4","C","D","9","+","+"]
 Output: 27
 Explanation:
@@ -42,7 +35,6 @@ Explanation:
 "+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
 The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
 Example 3:
-
 Input: ops = ["1","C"]
 Output: 0
 Explanation:
@@ -50,9 +42,7 @@ Explanation:
 "C" - Invalidate and remove the previous score, record is now [].
 Since the record is empty, the total sum is 0.
  
-
 Constraints:
-
 1 <= operations.length <= 1000
 operations[i] is "C", "D", "+", or a string representing an integer in the range [-3 * 104, 3 * 104].
 For operation "+", there will always be at least two previous scores on the record.
