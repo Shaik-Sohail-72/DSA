cee7bfd816f34cbcb1c95764c312d9b8fb30d8d5
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-04-24 11:55:45 +0530

    IMP - Graph - BFS - Implement BFS algorithm

---
+++ p131.java
@@ -0,0 +1,57 @@
+/*Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0.
+Note: One can move from node u to node v only if there's an edge from u to v and find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
+
+
+Example 1:
+
+Input:
+
+Output: 0 1 2 3 4
+Explanation: 
+0 is connected to 1 , 2 , 3.
+2 is connected to 4.
+so starting from 0, it will go to 1 then 2
+then 3.After this 2 to 4, thus bfs will be
+0 1 2 3 4.
+Example 2:
+
+Input:
+
+Output: 0 1 2
+Explanation:
+0 is connected to 1 , 2.
+so starting from 0, it will go to 1 then 2,
+thus bfs will be 0 1 2. 
+
+Your task:
+You dont need to read input or print anything. Your task is to complete the function bfsOfGraph() which takes the integer V denoting the number of vertices and adjacency list as input parameters and returns  a list containing the BFS traversal of the graph starting from the 0th vertex from left to right.
+
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V)
+
+
+Constraints:
+1 ≤ V, E ≤ 104*/
+class Solution {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[]=new boolean[V];
+        ArrayList<Integer> ls=new ArrayList<>();
+        bfs(adj,vis,0,ls);
+        return ls;
+    }
+    public static void bfs(ArrayList<ArrayList<Integer>> adj,boolean vis[],int start,ArrayList<Integer> ls){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(start);
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            if(vis[curr]==false){
+                ls.add(curr);
+                vis[curr]=true;
+                for(int dest:adj.get(curr)){
+                    q.add(dest);
+                }
+            }
+        }
+    }
+}
