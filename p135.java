a9c22492c31c89d0502a68bacebff2f46fd47005
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-04-26 12:23:16 +0530

    IMP - Graph - DFS - Detect Cycle in UnDirected Graph

--- p135.java
+++ p135.java
@@ -1,98 +1,63 @@
-// Undirected and unweighted graph
+/*Given an undirected graph with V vertices and E edges, check whether it contains any cycle or not. Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with.
 
-// input :-
-// vertices = 7
-// 0 1
-// 0 2
-// 1 0 
-// 1 3
-// 2 0
-// 2 4
-// 3 1
-// 3 4
-// 3 5
-// 4 2
-// 4 3
-// 4 5
-// 5 3
-// 5 4
-// 5 6
-// 6 5
-// BFS output :- 0 1 2 3 4 5 6
-// DFS output :- 0 1 3 4 2 5 6 
+Example 1:
 
-import java.util.*;
-public class BFS_DFS{
-    static class Edge{
-        int src;
-        int dest;
-        public Edge(int s,int d){
-            this.src=s;
-            this.dest=d;
-        }
-    }
-    public static void createGraph(ArrayList<Edge> graph[]){
-        for(int i=0;i<graph.length;i++){
-            graph[i]=new ArrayList<Edge>();
-        }
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+Input:  
+V = 5, E = 5
+adj = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}} 
+Output: 1
+Explanation: 
 
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+1->2->3->4->1 is a cycle.
+Example 2:
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
+Input: 
+V = 4, E = 2
+adj = {{}, {2}, {1, 3}, {2}}
+Output: 0
+Explanation: 
 
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
+No cycle in the graph.
+ 
 
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
+Your Task:
+You don't need to read or print anything. Your task is to complete the function isCycle() which takes V denoting the number of vertices and adjacency list as input parameters and returns a boolean value denoting if the undirected graph contains any cycle or not, return 1 if a cycle is present else return 0.
 
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
-        graph[5].add(new Edge(5, 6));
+NOTE: The adjacency list denotes the edges of the graph where edges[i] stores all other vertices to which ith vertex is connected.
 
-        graph[6].add(new Edge(6, 5));
-    }
-    public static void bfs(ArrayList<Edge> graph[],boolean vis[],int start){
-        Queue<Integer> q=new LinkedList<>();
-        q.add(start);
-        while(!q.isEmpty()){
-            int curr=q.remove();
-            if(vis[curr]==false){
-                System.out.print(curr+" ");
-                vis[curr]=true;
-                for(int i=0;i<graph[curr].size();i++){
-                    Edge e=graph[curr].get(i);
-                    q.add(e.dest);
+ 
+
+Expected Time Complexity: O(V + E)
+Expected Space Complexity: O(V)
+
+
+ 
+
+Constraints:
+1 ≤ V, E ≤ 105*/
+class Solution {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(vis[i]==false){
+                if(dfs(adj,vis,i,-1)){
+                    return true;
                 }
             }
         }
+        return false;
     }
-    public static void dfs(ArrayList<Edge> graph[],boolean vis[],int curr){  // O(V+E)
-        System.out.print(curr+" ");
+    public static boolean dfs(ArrayList<ArrayList<Integer>> adj,boolean vis[],int curr,int parent){
         vis[curr]=true;
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e=graph[curr].get(i);
-            if(vis[e.dest]==false){
-                dfs(graph,vis,e.dest);
+        for(int ele:adj.get(curr)){
+            if(vis[ele]==true && ele!=parent){
+                return true;
+            }else if(vis[ele]==false){
+                if(dfs(adj,vis,ele,curr)){
+                    return true;
+                }
             }
         }
-    }
-    public static void main(String[] args) {
-        int V=7;
-        ArrayList<Edge> graph[]=new ArrayList[V];
-        createGraph(graph);
-        boolean vis[]=new boolean[graph.length];
-        for(int i=0;i<V;i++){
-            if(vis[i]==false){
-                // bfs(graph,vis,i);
-                 dfs(graph,vis,i); 
-            }
-        }
+        return false;
     }
 }
